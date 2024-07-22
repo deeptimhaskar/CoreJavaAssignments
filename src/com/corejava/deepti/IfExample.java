2af3c0175808f52bed74ec17d28c06e4da7f21ac
DeeptiMhaskar <deeptiagre.mca@gmail.com>
2024-07-22 20:46:05 +0530

    test

---
+++ src/com/corejava/deepti/IfExample.java
@@ -0,0 +1,11 @@
+package com.corejava.deepti;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("IF Example");
+	}
+
+}
